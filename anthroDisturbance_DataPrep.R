f59a8a47174474c03f5c9170e6ddd554fb4076f5
tati-micheletti <tati.micheletti@gmail.com>
2024-06-04 09:53:58 +0000

    Release 1.0.0 Stable

--- anthroDisturbance_DataPrep.R
+++ anthroDisturbance_DataPrep.R
@@ -21,7 +21,7 @@ defineModule(sim, list(
                                 comment = NULL)), 
                       class = "person"),
   childModules = character(0),
-  version = list(anthroDisturbance_DataPrep = "0.0.1"),
+  version = list(anthroDisturbance_DataPrep = "1.0.0"),
   timeframe = as.POSIXlt(c(NA, NA)),
   timeunit = "year",
   citation = list("citation.bib"),
