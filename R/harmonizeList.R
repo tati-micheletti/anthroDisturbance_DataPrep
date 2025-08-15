50b85e3e594422a7a9b0f6e1156510e80275761b
RonCatK <ronald.kilian@protonmail.com>
2025-08-15 19:01:45 +0200

    corrected typo

---
+++ R/harmonizeList.R
@@ -0,0 +1,40 @@
+harmonizeList <- function(disturbances, whatNotToCombine){
+  if (is.null(disturbances)) stop("disturbances can't be NULL")
+  # Now that the datset is ready, we need to harmonize the ones with the same names
+  harmonizedList <- lapply(names(disturbances), function(sectors){
+    message(paste0("Harmonizing ", sectors))
+    DT <- disturbances[[sectors]]
+    if (suppressWarnings(all(all(unique(names(DT)) == names(DT)),
+                         length(unique(names(DT))) == length(names(DT))))){
+      message(crayon::green(paste0("Returning ", sectors, " unchanged.")))
+      return(DT)
+    } else {
+      toGoOver <- names(DT)[!grepl(x = names(DT),
+                                   pattern = paste(whatNotToCombine, collapse = "|"))]
+      # Subset without the whatNotToCombine and test again. If only one of each, 
+      # return the whole thing
+      toAddLater <- DT[!names(DT) %in% toGoOver]
+      toGoOverNow <- DT[names(DT) %in% toGoOver]
+      # Find out which names are repeated
+      dups <- unique(names(toGoOverNow)[duplicated(names(toGoOverNow))])
+      toAddLater <- c(toAddLater, toGoOverNow[!names(toGoOverNow) %in% dups])
+      if (length(dups) == 0){
+        DT <- c(toAddLater, toGoOverNow)
+        message(crayon::green(paste0("Returning ", sectors, " unchanged.")))
+        return(DT)
+      } else {
+        DT2 <- lapply(dups, function(grp){
+          allLays <- DT[names(DT) %in% grp]
+          names(allLays) <- NULL
+          oneLay <- do.call(rbind, allLays)
+          return(oneLay)
+        })
+        names(DT2) <- dups
+        message(crayon::yellow(paste0("Returning ", sectors, " merged.")))
+        return(c(toAddLater, DT2))
+      }
+    }
+  })
+  names(harmonizedList) <- names(disturbances)
+  return(harmonizedList)
+}
